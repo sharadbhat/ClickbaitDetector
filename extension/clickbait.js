ff37f4533910914e78825db6cd899da98fa1482b
Sharad Bhat <sharad.mbhat@gmail.com>
2019-02-07 23:22:57 +0530

    Create clickbait.js

---
+++ extension/clickbait.js
@@ -0,0 +1,22 @@
+// const url = "127:0:0:1:5000";
+// const data = {
+//   headlines: "Bank forecasts worst year for UK since 2009"
+// };
+
+alert("Hello");
+
+// params = {
+//   headers: {
+//     "content-type": "application/json"
+//   },
+//   body: data,
+//   method: "POST"
+// };
+
+// fetch(url, params)
+//   .then(info => {
+//     return info.json();
+//   })
+//   .then(res => {
+//     alert(res);
+//   });

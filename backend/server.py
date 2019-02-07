9d4eeb85ad0bce4f6d9a6a253d6f21d84ca8c068
Sharad Bhat <sharad.mbhat@gmail.com>
2019-02-07 23:22:27 +0530

    Added skeleton API code

---
+++ backend/server.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST"])
+def main_page():
+    response = app.response_class(
+        response=json.dumps("Hello"),
+        status=200,
+        mimetype="application/json"
+    )
+    data = request.get_json()
+    # print(data["headlines"])
+    # run_model(data["headlines"])
+    return response
+
+def run_model():
+    # Run trained model on new article
+    pass
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)

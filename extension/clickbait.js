b393b0c834f13d1edf7ea8bfa4fff6f4be19bb7f
Sharad Bhat <sharad.mbhat@gmail.com>
2019-02-07 23:48:53 +0530

    Update clickbait.js

--- extension/clickbait.js
+++ extension/clickbait.js
@@ -1,22 +1,23 @@
-// const url = "127:0:0:1:5000";
-// const data = {
-//   headlines: "Bank forecasts worst year for UK since 2009"
-// };
+func();
 
-alert("Hello");
+function func() {
+  const url = "127:0:0:1:5000";
+  const data = {
+    headlines: "Bank forecasts worst year for UK since 2009"
+  };
+  params = {
+    headers: {
+      "content-type": "application/json"
+    },
+    body: data,
+    method: "GET"
+  };
+  window.fetch(url)
+    .then(info => {
+      return info.json();
+    })
+    .then(res => {
+      alert(res);
+    });
+}
 
-// params = {
-//   headers: {
-//     "content-type": "application/json"
-//   },
-//   body: data,
-//   method: "POST"
-// };
-
-// fetch(url, params)
-//   .then(info => {
-//     return info.json();
-//   })
-//   .then(res => {
-//     alert(res);
-//   });

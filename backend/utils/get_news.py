fa402695c918bb5a272b61f75cc295a12c43de1e
sharadbhat <sharad.mbhat@gmail.com>
2019-02-15 13:07:09 +0530

    Update get_news.py

--- backend/utils/get_news.py
+++ backend/utils/get_news.py
@@ -1,9 +1,9 @@
 import requests
 from bs4 import BeautifulSoup as soup
 
-def get_news_from_keywords(headlines):
+def get_news_from_headlines(headlines):
   url = "https://news.google.com/rss/search?q=" + "+".join(headlines.split())
-  data = requests.get(url).content
+  data = requests.get(url).text
 
   parsed = soup(data, "lxml")
   news_list=parsed.findAll("item")

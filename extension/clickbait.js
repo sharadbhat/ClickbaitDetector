86fd12e548226848c63df636beeeece560bd33a7
sharadbhat <sharad.mbhat@gmail.com>
2019-02-15 13:07:59 +0530

    Update clickbait.js

--- extension/clickbait.js
+++ extension/clickbait.js
@@ -1,23 +1,10 @@
-func();
-
-function func() {
-  const url = "127:0:0:1:5000";
-  const data = {
-    headlines: "Bank forecasts worst year for UK since 2009"
-  };
-  params = {
-    headers: {
-      "content-type": "application/json"
-    },
-    body: data,
-    method: "GET"
-  };
-  window.fetch(url)
-    .then(info => {
-      return info.json();
-    })
-    .then(res => {
-      alert(res);
-    });
+function onGot(tabInfo) {
+  console.log(tabInfo);
 }
 
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+var gettingCurrent = browser.tabs.getCurrent();
+gettingCurrent.then(onGot, onError);

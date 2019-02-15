8c1e5d0ab6aa2a385ba902275955c52a17eb1c41
sharadbhat <sharad.mbhat@gmail.com>
2019-02-15 12:32:56 +0530

    Clean up html and get title and summary

--- backend/server.py
+++ backend/server.py
@@ -1,19 +1,24 @@
 from flask import Flask, request
+import requests
+from readability import Document
 import json
 
 app = Flask(__name__)
 
-# @app.route("/", methods=["POST"])
-@app.route("/")
+@app.route("/", methods=["POST"])
 def main_page():
     response = app.response_class(
         response=json.dumps("Hello"),
         status=200,
         mimetype="application/json"
     )
-    # data = request.get_json()
-    # print(data["headlines"])
-    # run_model(data["headlines"])
+    url = request.get_json()["url"]
+    page_content = requests.get(url).text
+    cleaned_page_content = Document(page_content)
+    headlines = cleaned_page_content.title()
+    summary = cleaned_page_content.summary()
+
+    # run_model(headlines, summary)
     return response
 
 def run_model():

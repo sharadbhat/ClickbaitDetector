262711522163c6d6a2323c3d9722156a396d21e9
Sharad Bhat <sharad.mbhat@gmail.com>
2019-03-15 01:47:01 +0530

    Update model.py

--- backend/utils/model.py
+++ backend/utils/model.py
@@ -1,14 +1,15 @@
 import os
 import gensim
 
+
 def load_model():
     try:
-        if not os.path.exists('../data/GoogleNews-vectors-negative300.bin.gz'):
-            raise ValueError("SKIP: You need to download the google news model")
+        if not os.path.exists('../models/GoogleNews-vectors-negative300.bin.gz'):
+            raise ValueError(
+                "SKIP: You need to download the google news model")
 
-        model =gensim.models.KeyedVectors.load_word2vec_format('../data/GoogleNews-vectors-negative300.bin.gz', binary=True)
+        model = gensim.models.KeyedVectors.load_word2vec_format(
+            '../models/GoogleNews-vectors-negative300.bin.gz', binary=True)
         return model
-    except MemoryError as memerror:
+    except MemoryError:
         print("You do not have enough RAM")
-    
-

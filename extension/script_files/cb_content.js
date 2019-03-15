acd220101aee4340a74179ec2fabc64aa96b9e7d
Tejesh <tejeshreddy111@gmail.com>
2019-03-15 13:09:45 +0530

    Update script_files/cb_contents.js

--- extension/script_files/cb_content.js
+++ extension/script_files/cb_content.js
@@ -1,1 +1,28 @@
 console.log("Content Script Loaded")
+window.addEventListener("onmouseover",wordSelected(this))
+function wordSelected(nodeReference)
+{
+	/*let word=window.getSelection().toString();
+	console.log(word)*/
+	/*text = window.nodeReference.innerText.toString();
+    console.log('The text is: ' + text);
+    hr=nodeReference.href;
+    console.log(hr);
+    */
+
+    var list = document.querySelectorAll( "a" );
+    for ( var i = 0; i < list.length; i ++)
+      list.item(i).onmouseover = function() {  
+      	var passedLink=this.href;
+      	//console.log(passedLink);
+      	if(passedLink.length>0)
+      	{
+      		let message={
+      			link : passedLink
+      		};
+      		console.log(message.link )
+      		//chrome.runtime.sendMessage(message)
+      	}
+
+      };
+}

eca9cd61ad91da398001dede81846d509aad0d7d
Tejesh <tejeshreddy111@gmail.com>
2019-03-24 11:53:45 +0530

    Background script updated

--- extension/script_files/cb_background.js
+++ extension/script_files/cb_background.js
@@ -1,1 +1,8 @@
 console.log("Background script loaded")
+
+
+chrome.runtime.onMessage.addListener(receiver);
+function receiver(request,sender,sendResponse){
+	console.log(request.link);
+	window.link=request.link;
+}

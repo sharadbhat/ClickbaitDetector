82d09691fd651a7ab9bc4c9c9b13000d124f5fa5
Tejesh <tejeshreddy111@gmail.com>
2019-03-28 23:06:21 +0530

    Update popup_script.js

--- extension/popup/popup_script.js
+++ extension/popup/popup_script.js
@@ -1,6 +1,10 @@
 function setup(){
 	noCanvas();
 	let bgpage=chrome.extension.getBackgroundPage();
-	let link=bgpage.link;
-	createP(link);
+	
+	/*let link=bgpage.link;
+	createP(link);*/
+
+	let msg=bgpage.msg;
+	
 }

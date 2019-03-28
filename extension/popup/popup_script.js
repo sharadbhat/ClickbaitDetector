c0324a287f9f8d0a10d7b88502ab488bd8486b68
Tejesh <tejeshreddy111@gmail.com>
2019-03-29 00:17:46 +0530

    Update popup_script.js

--- extension/popup/popup_script.js
+++ extension/popup/popup_script.js
@@ -12,4 +12,11 @@ function setup(){
 	let random_percent = Math.random(0,1);
 	createP("Percent: "+random_percent*100)
 
+	if((random_percent*100)<40){
+		createP("It's a Clickbait")
+	}
+	else{
+		createP("It's not a Clickbait")
+	}
+
 }

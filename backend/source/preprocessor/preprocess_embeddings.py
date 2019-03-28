84b28adbfd6806aaaaa76c5c3869e7110d346fce
Sharad Bhat <sharad.mbhat@gmail.com>
2019-03-28 21:35:08 +0530

    Update preprocess_embeddings.py

--- backend/source/preprocessor/preprocess_embeddings.py
+++ backend/source/preprocessor/preprocess_embeddings.py
@@ -3,31 +3,26 @@ from sklearn.decomposition import PCA
 
 
 def preprocess_embeddings(embedding_dimension, vocabulary):
-    """
-    Reads GloVe file and returns weights for each word in vocabulary.
-    """
+
     embeddings = {}
-    with open("models/glove.6B.50d.txt", encoding="utf8") as glove_file:
-        for line in glove_file:
+    with open("models/glove.6B.50d.txt") as glove_file:
+        for line in tqdm.tqdm(glove_file, desc="reading embeddings", total=400000):
             start = line.find(" ")
             word = line[:start]
             embeddings[word] = np.fromstring(
                 line[start:], sep=" ", dtype=np.float32)
 
-    weights = np.zeros((len(vocabulary), 50))  # Glove 50 D
+    weights = np.zeros((len(vocabulary), 50))  # existing vectors are 50-D
     for i, word in enumerate(vocabulary):
         if word in embeddings:
             weights[i] = embeddings[word]
 
-    pca = PCA(n_components=embedding_dimension)
+    pca = PCA(n_components=EMBEDDING_DIMENSION)
     weights = pca.fit_transform(weights)
     return weights
 
 
 if __name__ == "__main__":
-    """
-    Preprocesses the GloVe embeddings.
-    """
     EMBEDDING_DIMENSION = 30
     vocabulary = open("data/vocabulary.txt").read().split("\n")
     weights = preprocess_embeddings(EMBEDDING_DIMENSION, vocabulary)

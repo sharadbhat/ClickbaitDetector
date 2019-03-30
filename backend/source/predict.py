70b5d1e0a5abf1f41ea8f8ee732f24de00d44fc4
Sharad Bhat <sharad.mbhat@gmail.com>
2019-03-31 00:38:23 +0530

    Update predict.py

--- backend/source/predict.py
+++ backend/source/predict.py
@@ -12,7 +12,6 @@ from models.cnn import ConvolutionalNet
 
 # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-MATCH_MULTIPLE_SPACES = re.compile("\ {2,}")
 SEQUENCE_LENGTH = 20
 EMBEDDING_DIMENSION = 30
 

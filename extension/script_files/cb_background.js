7a5bab1595f2579495678f6e66023e4ef4ebdfaf
tejeshreddy <tejeshreddy111@gmail.com>
2019-04-17 16:30:17 +0530

    Update Background_script.js

--- extension/script_files/cb_background.js
+++ extension/script_files/cb_background.js
@@ -2,8 +2,8 @@ console.log("Background script loaded")
 
 // chrome.runtime.onMessage.addListener(receiver);
 // function receiver(request,sender,sendResponse){
-// 	console.log(request.link);
-// 	window.link=request.link;
+// console.log(request.link);
+// window.link=request.link;
 // }
 
 var contextMenuItem = {

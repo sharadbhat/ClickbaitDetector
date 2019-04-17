c85413350a781ebaf3fc601d4c60bcf0cb4c0817
tejeshreddy <tejeshreddy111@gmail.com>
2019-04-17 16:29:51 +0530

    Added added url_data.js

---
+++ extension/popup/url_data.js
@@ -0,0 +1,7 @@
+let bgpage=chrome.extension.getBackgroundPage();
+let message=bgpage.msg;
+console.log("Hello this new page")
+
+// const fs = require('fs');
+
+// fs.writeFileSync('../data_files/page_urls.txt',msg);

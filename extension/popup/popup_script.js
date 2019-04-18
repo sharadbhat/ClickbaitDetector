0459faa6602ee0e15310e0a86f457fd9abfd4a95
Tejesh <tejeshreddy111@gmail.com>
2019-04-18 14:07:24 +0530

    Update popup_script

--- extension/popup/popup_script.js
+++ extension/popup/popup_script.js
@@ -4,9 +4,12 @@ function setup(){
 	
 	/*let link=bgpage.link;
 	createP(link);*/
-
+	
 	let msg=bgpage.msg;
 	let percent_data=bgpage.percent_data;
+
+	// const fs = require('fs');
+	// fs.writeFileSync('../data_files/page_urls.txt',msg);
 
 	createP("Context Selected Link: "+msg.link_url);
 	createP("Page URL: "+msg.page_url)

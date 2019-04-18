879b316392719039fbfeb29dd1df2adf823c620c
Sharad Bhat <sharad.mbhat@gmail.com>
2019-04-19 01:24:45 +0530

    Update similarity.py

--- backend/source/utils/similarity.py
+++ backend/source/utils/similarity.py
@@ -1,23 +1,30 @@
-from model import load_model
 from nltk.corpus import stopwords
-from nltk import download
-# download('stopwords')  # Download stopwords list.
+from gensim import corpora
+import gensim.downloader as api
+from gensim.models import WordEmbeddingSimilarityIndex
+from gensim.similarities import SparseTermSimilarityMatrix
 
-def preprocess(s1,s2):
-    s1 = s1.lower().split()
-    s2 = s2.lower().split()
-    stop_words = stopwords.words('english')
-    s1 = [w for w in s1 if w not in stop_words]
-    s2 = [w for w in s2 if w not in stop_words]
-    return s1,s2
+class Similarity:
+    def __init__(self):
+        self.stop_words = stopwords.words('english')
+        self.w2v_model = api.load("glove-wiki-gigaword-50")
+        self.similarity_index = WordEmbeddingSimilarityIndex(self.w2v_model)
 
-def get_similarity(s1,s2):
-    s1,s2 = preprocess(s1,s2)
-    model = load_model()
-    if not model:
-        print("error loading model")
-        exit()
-
-    distance = model.wmdistance(s1, s2)
-    return distance
-
+    def make_document(self, headline, articles):
+        temp = []
+        headline = [w for w in headline.lower().split() if w not in self.stop_words]
+        for article in articles:
+            article = [w for w in article.lower().split() if w not in self.stop_words]
+            temp.append(article)
+        self.documents = [headline] + temp
+        dictionary = corpora.Dictionary(self.documents)
+        self.similarity_matrix = SparseTermSimilarityMatrix(self.similarity_index, dictionary)
+        headline = dictionary.doc2bow(headline)
+        articles = [dictionary.doc2bow(i) for i in temp]
+        similarities = []
+        for i in articles:
+            similarities.append(self.get_similarity(headline, i))
+    
+    def get_similarity(self, s1, s2):
+        return self.similarity_matrix.inner_product(s1, s2, normalized=True)
+    

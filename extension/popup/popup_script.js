98f1b737603607286c00f105ac251bebbf35a13a
Tejesh <tejeshreddy111@gmail.com>
2019-04-18 14:00:07 +0530

    Update pop_up

--- extension/popup/popup_script.js
+++ extension/popup/popup_script.js
@@ -6,17 +6,18 @@ function setup(){
 	createP(link);*/
 
 	let msg=bgpage.msg;
+	let percent_data=bgpage.percent_data;
+
 	createP("Context Selected Link: "+msg.link_url);
 	createP("Page URL: "+msg.page_url)
 
-	let random_percent = Math.random(0,1);
-	createP("Percent: "+random_percent*100)
+	createP("Percent: "+ percent_data.cb_percentage);
 
-	if((random_percent*100)<40){
-		createP("It's not Clickbait")
-	}
-	else{
-		createP("It's a Clickbait")
-	}
+	// if(cb_percent<40){
+	// 	createP("It's not Clickbait")
+	// }
+	// else{
+	// 	createP("It's a Clickbait")
+	// }
 
 }

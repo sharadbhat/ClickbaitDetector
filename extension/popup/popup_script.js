365020d11486d36c533d2b11ea2a5f9d4b5af300
Tejesh <tejeshreddy111@gmail.com>
2019-04-20 20:53:27 +0530

    Update popup_script.js

--- extension/popup/popup_script.js
+++ extension/popup/popup_script.js
@@ -13,13 +13,26 @@ function setup(){
 
 	createP("Context Selected Link: "+msg.link_url);
 	createP("Page URL: "+msg.page_url)
-	createP("Percent: "+ percent_data.cb_percentage);
+	
+	var youtube_text=/youtube/
+	var facebook_text=/facebook/
 
-	if(percent_data.cb_percentage<40){
-		createP("It's not Clickbait")
+	if((msg.link_url).match(youtube_text))
+	{
+		createP("This clickbait detector doesn't support Youtube links!!")
 	}
-	else{
-		createP("It's a Clickbait")
+	else if((msg.link_url).match(facebook_text))
+	{
+		createP("This clickbait detector doesn't support Facebook links!!")
 	}
-
+	else
+	{
+		createP("Percent: "+ percent_data.cb_percentage);
+		if(percent_data.cb_percentage<40){
+			createP("It's not Clickbait")
+		}
+		else{
+			createP("It's a Clickbait")
+		}
+	}
 }

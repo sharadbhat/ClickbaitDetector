523dfb93a3452e02d8511177f93acc650fc9725c
Sharad Bhat <sharad.mbhat@gmail.com>
2019-04-24 02:01:19 +0530

    Update server.py

--- backend/source/server.py
+++ backend/source/server.py
@@ -5,11 +5,12 @@ import json
 from subprocess import check_output
 
 from utils.get_news import get_news_from_headlines
+from utils.similarity import Similarity
 
 app = Flask(__name__)
 
-
-@app.route("/", methods=["POST"])
+# @app.route("/", methods=["POST"])
+@app.route("/", methods=["GET"])
 def main_page():
     """
         - Handles incoming request from extension.
@@ -17,9 +18,10 @@ def main_page():
         - Cleans up HTML using Python Readability.
         - Sends headlines and summary to model.
     """
-    url = request.get_json()["url"]
+    headline = request.args.get("headline")
+    # url = request.get_json()["url"]
 
-    page_content = requests.get(url).text
+    page_content = requests.get(headline).text
 
     cleaned_page_content = Document(page_content)
     headline = cleaned_page_content.title()
@@ -27,7 +29,8 @@ def main_page():
     percentage = run_model(headline)
 
     response = app.response_class(
-        response=json.dumps({"url": url, "headline": headline, "percentage": percentage}),
+        response=json.dumps({"headline": headline, "percentage": percentage}),
+        # response=json.dumps({"url": url, "headline": headline, "percentage": percentage}),
         status=200,
         mimetype="application/json"
     )
@@ -39,22 +42,27 @@ def run_model(headline):
         - Run trained model on new article.
         - Passes headlines and summary to model.
     """
-    # TODO: Call model with headlines.
     val = check_output("python source\predict.py \"{}\"".format(headline))
-    return float(val.decode("utf8").replace("\r\n", ""))
-    # compare_similar_news(headlines)
-    pass
+    val = float(val.decode("utf8").replace("\r\n", ""))
+    print(val)
+    if val > 60.0 or val < 30.0:
+        return val
+    else:
+        similarity_score = compare_similar_news(headline)
+        print(similarity_score)
+        return val
 
 
-def compare_similar_news(headlines):
+
+def compare_similar_news(headline):
     """
         - Calls utility function to get similar articles from Google News.
         - Compares news to check how similar the articles are.
         - Only used if clickabit detected percentage is low.
     """
-    similar_articles = get_news_from_headlines(headlines)
-    # TODO: Compare articles
-    pass
+    similar_articles = get_news_from_headlines(headline)
+    similarity_score = Similarity().make_document(headline, similar_articles)
+    return similarity_score
 
 
 if __name__ == "__main__":

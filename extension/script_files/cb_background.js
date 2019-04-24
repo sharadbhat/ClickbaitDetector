82b1d62452de26aa0dda29245ae1c2664e0c3620
Sharad Bhat <sharad.mbhat@gmail.com>
2019-04-24 23:19:28 +0530

    Update cb_background.js

--- extension/script_files/cb_background.js
+++ extension/script_files/cb_background.js
@@ -2,7 +2,7 @@ console.log("Background script loaded");
 
 var contextMenuItem = {
   id: "Clickbait",
-  title: "Check if Clickbait or Not",
+  title: "Check if clickbait or not",
   contexts: ["link"]
 };
 
@@ -13,57 +13,47 @@ chrome.contextMenus.onClicked.addListener(function(clickData) {
     console.log("Context Selection taken place");
     console.log("Link Url: " + clickData.linkUrl);
     console.log("Page Url: " + clickData.pageUrl);
-
     var request_contextlink = new XMLHttpRequest();
     request_contextlink.onreadystatechange = function() {
       if (request_contextlink.readyState == 4) {
         var data = JSON.parse(request_contextlink.responseText);
         window.clickbait = data.percentage.toString();
         var alert_data = "Headline: " + data.headline;
-		alert_data += "\nPercentage: " + data.percentage.toString();
-		if (data.percentage < 50.0) {
-			alert_data += "\nResult: Not a clickbait.";
-		}
-		else {
-			alert_data += "\nResult: Clickbait.";
-		}
+        alert_data += "\nPercentage: " + data.percentage.toString();
+        if (data.percentage > 70.0) {
+          alert_data += "\nResult: Clickbait.";
+        } else if (data.percentage > 50.0) {
+          alert_data += "\nResult: Probably clickbait.";
+        } else if (data.percentage > 30.0) {
+          alert_data += "\nResult: Probably not clickbait.";
+        } else {
+          alert_data += "\nResult: Not clickbait.";
+        }
         alert(alert_data);
       }
     };
 
-
     var request_pagelink = new XMLHttpRequest();
-    request__pagelink.onreadystatechange = function() {
+    request_pagelink.onreadystatechange = function() {
       if (request__pagelink.readyState == 4) {
         var data = JSON.parse(request__pagelink.responseText);
         window.clickbait = data.percentage.toString();
-          window.percent_data = {
+        window.percent_data = {
           cb_percentage: clickbait
         };
       }
     };
- 
 
     window.msg = {
       link_url: clickData.linkUrl,
       page_url: clickData.pageUrl
     };
 
-    
     request_contextlink.open(
       "GET",
       "http://localhost:5000/?headline=" + clickData.linkUrl,
       false
     );
     request_contextlink.send();
-
-    request_pagelink.open(
-      "GET",
-      "http://localhost:5000/?headline=" + clickData.pageUrl,
-      false
-    );
-    request_pagelink.send();
-
-    // chrome.storage.local.set({'link_url':clickData.link_url});
   }
 });

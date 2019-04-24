3fc96a6820a740ff54d6d19f6fbc2ea7649ed64c
Sharad Bhat <sharad.mbhat@gmail.com>
2019-04-25 00:52:49 +0530

    Update cb_background.js

--- extension/script_files/cb_background.js
+++ extension/script_files/cb_background.js
@@ -13,7 +13,7 @@ chrome.contextMenus.onClicked.addListener(function(clickData) {
       if (request_contextlink.readyState == 4) {
         var data = JSON.parse(request_contextlink.responseText);
         var alert_data = "";
-        if (data.error !== "undefined") {
+        if (data.error !== undefined) {
           alert_data += "Error: " + data.error;
         } else {
           alert_data += "Headline: " + data.headline;
@@ -21,8 +21,14 @@ chrome.contextMenus.onClicked.addListener(function(clickData) {
           if (data.percentage > 70.0) {
             alert_data += "\nResult: Clickbait.";
           } else if (data.percentage > 50.0) {
+            similar = data.similarArticles.toString();
+            similar = similar.charAt(0).toUpperCase() + similar.slice(1);
+            alert_data += "\nSimilar articles found: " + similar;
             alert_data += "\nResult: Probably clickbait.";
           } else if (data.percentage > 30.0) {
+            similar = data.similarArticles.toString();
+            similar = similar.charAt(0).toUpperCase() + similar.slice(1);
+            alert_data += "\nSimilar articles found: " + similar;
             alert_data += "\nResult: Probably not clickbait.";
           } else {
             alert_data += "\nResult: Not clickbait.";
